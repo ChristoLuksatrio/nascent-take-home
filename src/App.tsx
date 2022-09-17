171747c3cbb8737861cadd44924b18877dcdd1de
ChristoLuksatrio <christo.luksatrio@gmail.com>
2022-09-17 11:42:30 -0400

    connected frontend with backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { GET_SUBMISSIONS } from "./graphql/Query";
+import { useQuery } from "@apollo/client";
 
 function App() {
+  const { loading, error, data } = useQuery(GET_SUBMISSIONS);
+  if (loading) {
+    return <p>loading</p>;
+  }
+  if (error) {
+    return <div>Error</div>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data?.getSubmissions.map((submission: any, index: number) => (
+        <div key={index}>{submission.firstName}</div>
+      ))}
     </div>
   );
 }
