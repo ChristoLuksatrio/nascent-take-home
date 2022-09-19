5c55178ff337a0035a7da52a9e8ffb180b260181
ChristoLuksatrio <christo.luksatrio@gmail.com>
2022-09-19 08:09:03 -0400

    added verification page

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import Button from "../components/Button";
 
 const Verify = () => {
+  const formInput = JSON.parse(window.localStorage.getItem("formInput") || "");
   return (
     <div>
       <p>Please verify your info</p>
+      {Object.keys(formInput).map((key, index) => (
+        <p key={index}>
+          {key}:{formInput[key]}
+        </p>
+      ))}
       <Button text="Confirm" path="/success" />
     </div>
   );
