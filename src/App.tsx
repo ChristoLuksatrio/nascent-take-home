73c805d62735f06ac7e6cbec0d219f7b14cb41e4
ChristoLuksatrio <christo.luksatrio@gmail.com>
2022-09-19 23:54:09 -0400

    removed comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-// import { ADD_SUBMISSION } from "./graphql/Mutation";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Registration from "./pages/Registration";
@@ -10,21 +9,6 @@ import Success from "./pages/Success";
 import { Infoboard } from "./pages/Infoboard";
 
 function App() {
-  // const [addSubmission] = useMutation(ADD_SUBMISSION);
-
-  // const clickerino = () => {
-  //   addSubmission({
-  //     variables: {
-  //       firstName: "test",
-  //       lastName: "test",
-  //       phoneNumber: "test",
-  //       address: "test",
-  //       pokemon: "test",
-  //     },
-  //     refetchQueries: [{ query: GET_SUBMISSIONS }],
-  //   });
-  // };
-
   return (
     <div className="flex flex-col items-center h-screen bg-gray-800 text-white pt-16 px-4">
       <div className="card bg-amber-100 p-4 border-2 border-amber-800 rounded-lg text-black">
