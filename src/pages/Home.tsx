f322fe7d83f4f24727e600ec44fae44ecc860b31
ChristoLuksatrio <christo.luksatrio@gmail.com>
2022-09-19 23:00:18 -0400

    added logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Button from "../components/Button";
+import logo from "../assets/logo.webp";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-full max-w-xl text-center">
+      <div className="w-20 mb-4">
+        <img className="h-auto w-auto" src={logo} alt="pokeball" />
+      </div>
+      <h1 className="mb-4">Nascent Pokemon Tech Challenge</h1>
       <Button className="mb-4" text="START" path="/signup" />
       <em className="text-xs justify-self-end">
         This website was made for Nascent's technical challenge and is purely
